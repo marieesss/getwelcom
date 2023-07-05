9e37194e86c02f9dcb43c8bea69189be80a43a5b
Marie <mje.espinosa@gmail.com>
2023-07-05 16:06:54 +0200

    add menu

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { BrowserRouter} from 'react-router-dom';
 import Home from './components/Home/Home';
-import Playlist from './components/Playlist';
+import Playlist from './components/Playlist/Playlist';
+import Menu from './components/Layout/Menu';
 
 function App() {
 
   return (
+    <div>
+    <Menu/>
     <BrowserRouter>
       <Routes>
             <Route path="/" element={<Home/>} />
             <Route path="*" element={<Home/>} />
-            <Route path="/playlist/:id" element={<Playlist/>} />
+            <Route path="/playlist" element={<Playlist/>} />
             </Routes>
     </BrowserRouter>
+    </div>
   );
 }
 
